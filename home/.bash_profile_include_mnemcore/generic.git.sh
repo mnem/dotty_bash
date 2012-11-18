f9ec7a1647102b606e59554bfa7151e171b314f0
David Wagner <mnem@noiseandheat.com>
2012-11-18 14:54:06 +0000

    Make the hostname stand out more in the prompt and apply an xmas theme

--- home/.bash_profile_include_mnemcore/generic.git.sh
+++ home/.bash_profile_include_mnemcore/generic.git.sh
@@ -2,10 +2,13 @@
 # Setup the prompt so that it shows the current branch when in a git directory.
 #
 # Requires git-completion (and git-prompt if it exists) to be sourced somewhere
-local      BLUE="\[\e[0;34m\]"
-local       RED="\[\e[0;31m\]"
-local    YELLOW="\[\e[0;33m\]"
-local    PURPLE="\[\e[0;35m\]"
+local BLUE="\[\e[0;34m\]"
+local RED="\[\e[0;31m\]"
+local YELLOW="\[\e[0;33m\]"
+local PURPLE="\[\e[0;35m\]"
+local WHITE="\[\e[0;37m\]"
+local ON_BLUE="\[\e[0;44m\]"
+
 local CLR_RESET="\[\e[0m\]"
 
 case $TERM in
@@ -17,6 +20,6 @@ case $TERM in
   ;;
 esac
 
-PS1="${TITLEBAR}🎃  $RED\u@\h $PURPLE\w$YELLOW\$(__git_ps1)$CLR_RESET\n👻  \$ "
+PS1="${TITLEBAR}🎅  $RED\u@$WHITE$ON_BLUE\h$CLR_RESET $PURPLE\w$YELLOW\$(__git_ps1)$CLR_RESET\n🎄  \$ "
 PS2='> '
 PS4='+ '
