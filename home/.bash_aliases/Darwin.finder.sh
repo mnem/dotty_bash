be0f1d447afdea0d734cd0b99fc3f7fe37d9b8a1
David Wagner <mnem@noiseandheat.com>
2013-02-20 12:49:30 +0000

    Alias to quickly fix 'Open with…' dupes

--- home/.bash_aliases/Darwin.finder.sh
+++ home/.bash_aliases/Darwin.finder.sh
@@ -1,2 +1,4 @@
-alias finder_showall='defaults write com.apple.finder AppleShowAllFiles -bool YES'
-alias finder_showsome='defaults write com.apple.finder AppleShowAllFiles -bool NO'
+alias finder-showall='defaults write com.apple.finder AppleShowAllFiles -bool YES'
+alias finder-showsome='defaults write com.apple.finder AppleShowAllFiles -bool NO'
+alias finder-fix-open-with='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
+
