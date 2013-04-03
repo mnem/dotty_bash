072289d871ef4b9f11c19c05e588ce7ba2830b1f
David Wagner <mnem@noiseandheat.com>
2013-04-03 11:59:12 +0100

    Git prompt for EC2

--- home/.bash_profile_include_mnemcore/Linux.git.sh
+++ home/.bash_profile_include_mnemcore/Linux.git.sh
@@ -1,3 +1,4 @@
 ############################################################################
 # Using this as the name for my EC2 instance. Not really ideal.
 source_files /usr/share/doc/git-1.8.1.4/contrib/completion/git-completion.bash
+source_files /usr/share/doc/git-1.8.1.4/contrib/completion/git-prompt.sh
