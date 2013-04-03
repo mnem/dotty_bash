108f0267f9ac36fb8e8f2927256332b5e998272d
David Wagner <mnem@noiseandheat.com>
2013-04-03 11:52:06 +0100

    Update EC2 git completion path

--- home/.bash_profile_include_mnemcore/Linux.git.sh
+++ home/.bash_profile_include_mnemcore/Linux.git.sh
@@ -1,3 +1,3 @@
 ############################################################################
 # Using this as the name for my EC2 instance. Not really ideal.
-source_files /usr/share/doc/git-1.7.4.5/contrib/completion/git-completion.bash
+source_files /usr/share/doc/git-1.8.1.4/contrib/completion/git-completion.bash
