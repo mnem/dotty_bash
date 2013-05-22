2ee942bdaf90ee0cf61f255c1c2e3cc2412338b2
David Wagner <mnem@noiseandheat.com>
2013-05-22 11:23:42 +0100

    Bees!

--- home/.bash_profile_include_mnemcore/generic.git.sh
+++ home/.bash_profile_include_mnemcore/generic.git.sh
@@ -26,7 +26,7 @@ case $TERM in
 esac
 
 THEME_A='🌸'
-THEME_B='🐣'
+THEME_B='🐝'
 
 PS1="${TITLEBAR}$THEME_A  $RED\u@$GREEN$ON_RED\h$CLR_RESET$YELLOW\$(__git_ps1) $PURPLE\w$CLR_RESET\n$THEME_B  \$ "
 PS2='> '
