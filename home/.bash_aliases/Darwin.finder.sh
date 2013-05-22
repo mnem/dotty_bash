e78c6a59bf929d3095734cb1cbb087fc27c710ab
David Wagner <mnem@noiseandheat.com>
2013-05-22 11:23:27 +0100

    Restart UIServer after changing hidden file visibility

--- home/.bash_aliases/Darwin.finder.sh
+++ home/.bash_aliases/Darwin.finder.sh
@@ -1,4 +1,5 @@
-alias finder-showall='defaults write com.apple.finder AppleShowAllFiles -bool YES'
-alias finder-showsome='defaults write com.apple.finder AppleShowAllFiles -bool NO'
+alias finder-showall='defaults write com.apple.finder AppleShowAllFiles -bool YES;killall SystemUIServer'
+alias finder-showsome='defaults write com.apple.finder AppleShowAllFiles -bool NO;killall SystemUIServer'
 alias finder-fix-open-with='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
-
+alias finder-hide-window-shadows='defaults write com.apple.screencapture disable-shadow -bool true;killall SystemUIServer'
+alias finder-show-window-shadows='defaults write com.apple.screencapture disable-shadow -bool false;killall SystemUIServer'
