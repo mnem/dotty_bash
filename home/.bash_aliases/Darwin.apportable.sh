ef752d77d2f82f675974738bf827e54d93baeebe
David Wagner <mnem@noiseandheat.com>
2013-10-12 12:16:22 +0100

    Make the apportable-activate function explain what it's doing and make it activate Xcode 4

--- home/.bash_aliases/Darwin.apportable.sh
+++ home/.bash_aliases/Darwin.apportable.sh
@@ -1,1 +1,11 @@
-alias apportable-activate='export PATH="/Users/mnem/.apportable/SDK/bin:$PATH" && unset ANDROID_SDK ANDROID_NDK'
+apportable-activate() {
+	local APPORTABLE_BIN=/Users/mnem/.apportable/SDK/bin
+	echo Adding $APPORTABLE_BIN to start of PATH
+	export PATH="$APPORTABLE_BIN:$PATH"
+
+	echo Unsetting ANDROID_SDK and ANDROID_NDK
+	unset ANDROID_SDK ANDROID_NDK
+
+	echo Activating Xcode 4
+	sudo xcode-select --switch /Applications/Xcode4.app/Contents/Developer
+}
