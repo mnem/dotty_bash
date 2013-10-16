e1c136aa15d00eef2e99356433cf4f9ac1657cd8
David Wagner <mnem@noiseandheat.com>
2013-10-16 21:32:41 +0100

    Stop deactivating Xcode 5 for apportable as they now support it.

--- home/.bash_aliases/Darwin.apportable.sh
+++ home/.bash_aliases/Darwin.apportable.sh
@@ -5,7 +5,4 @@ apportable-activate() {
 
 	echo Unsetting ANDROID_SDK and ANDROID_NDK
 	unset ANDROID_SDK ANDROID_NDK
-
-	echo Activating Xcode 4
-	sudo xcode-select --switch /Applications/Xcode4.app/Contents/Developer
 }
