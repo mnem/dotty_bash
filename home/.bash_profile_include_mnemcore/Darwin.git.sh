0162be3999f0f131de54f0c2ac81ec7197094997
David Wagner <mnem@noiseandheat.com>
2014-01-02 11:55:13 +0000

    Load all extra autocompletion files in local/etc/autocompletion.d

--- home/.bash_profile_include_mnemcore/Darwin.git.sh
+++
@@ -1,4 +0,0 @@
-############################################################################
-# brew install git-extras
-source_files /usr/local/etc/bash_completion.d/git-completion.bash
-source_files /usr/local/etc/bash_completion.d/git-prompt.sh
