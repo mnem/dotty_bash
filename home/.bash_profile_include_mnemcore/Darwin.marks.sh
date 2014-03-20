5223fe2e754b8f525ac65eb734fd124e579a2213
David Wagner <mnem@noiseandheat.com>
2014-03-20 16:27:14 +0000

    Added marks/jump bash functions

---
+++ home/.bash_profile_include_mnemcore/Darwin.marks.sh
@@ -0,0 +1,26 @@
+# From:
+#   http://jeroenjanssens.com/2013/08/16/quickly-navigate-your-filesystem-from-the-command-line.html
+#
+export MARKPATH=$HOME/.marks
+
+function jump {
+    cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
+}
+function mark {
+    mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1"
+}
+function unmark {
+    rm -i "$MARKPATH/$1"
+}
+function marks {
+    ls -l "$MARKPATH" | tail -n +2 | awk '{printf "%-15s -> %s\n", $9,$11}'
+}
+
+_completemarks() {
+  local curw=${COMP_WORDS[COMP_CWORD]}
+  local wordlist=$(find $MARKPATH -type l | xargs basename)
+  COMPREPLY=($(compgen -W '${wordlist[@]}' -- "$curw"))
+  return 0
+}
+
+complete -F _completemarks jump unmark
